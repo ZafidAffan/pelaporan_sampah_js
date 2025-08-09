27dbc81d798bfc59150f7a53aba25cf18e5c5e62
ZafidAffan <155897126+ZafidAffan@users.noreply.github.com>
2025-08-09 19:58:56 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,15 @@
 // firebase.js
 const admin = require('firebase-admin');
-const path = require('path');
 
-const serviceAccount = require('pelaporan_sampah_api/serviceAccountKey(sampah).json'); // letakkan JSON key kamu di sini
+// Ambil key dari environment variable di Railway
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: 'sampah-c8634.appspot.com'
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET
 });
 
+// Buat bucket untuk akses storage
 const bucket = admin.storage().bucket();
 
 module.exports = bucket;
